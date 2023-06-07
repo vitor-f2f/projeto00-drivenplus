3c7ecaad945b7d92613dd7fc452eb7ddd512dbfa
vitor-driven <38925839+vitor-driven@users.noreply.github.com>
2023-06-06 21:43:39 -0300

    signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,17 +1,227 @@
 import styled from "styled-components";
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function SignUp() {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const [signupCpf, setCpf] = useState("");
+    const [signupEmail, setEmail] = useState("");
+    const [signupName, setName] = useState("");
+    const [signupPassword, setPassword] = useState("");
+    const signupObj = {
+        cpf: "",
+        email: "",
+        name: "",
+        password: "",
+    };
+
+    const [alertMsg, setAlert] = useState("");
+    const [showAlert, setShowAlert] = useState(false);
+
+    function formatCpf(value) {
+        let cpf = value.replace(/\D/g, "").slice(0, 11);
+        switch (cpf.length) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                break;
+            case 4:
+            case 5:
+            case 6:
+                cpf = cpf.substring(0, 3) + "." + cpf.substring(3);
+                break;
+            case 7:
+            case 8:
+            case 9:
+                cpf =
+                    cpf.substring(0, 3) +
+                    "." +
+                    cpf.substring(3, 6) +
+                    "." +
+                    cpf.substring(6);
+                break;
+            default:
+                cpf =
+                    cpf.substring(0, 3) +
+                    "." +
+                    cpf.substring(3, 6) +
+                    "." +
+                    cpf.substring(6, 9) +
+                    "-" +
+                    cpf.substring(9);
+                break;
+        }
+        return cpf;
+    }
+
+    function validateEmail(email) {
+        const emailRegex =
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        return emailRegex.test(email);
+    }
+
+    function sendSignup() {
+        if (
+            signupCpf === "" ||
+            signupEmail === "" ||
+            signupName === "" ||
+            signupPassword === ""
+        ) {
+            setAlert("Preencha todos os dados para prosseguir.");
+            setShowAlert(true);
+            return;
+        }
+        if (!validateEmail(signupEmail)) {
+            setAlert("Insira um endereço de e-mail válido.");
+            setShowAlert(true);
+            return;
+        }
+        setLoading(true);
+        signupObj.cpf = signupCpf;
+        signupObj.email = signupEmail;
+        signupObj.name = signupName;
+        signupObj.password = signupPassword;
+        console.log(signupObj);
+        const promise = axios.post(
+            "https://mock-api.driven.com.br/api/v4/driven-plus/auth/sign-up",
+            signupObj
+        );
+        promise
+            .then(() => {
+                setLoading(false);
+                navigate("/");
+            })
+            .catch((err) => {
+                setLoading(false);
+                console.log(err);
+                setAlert(`Erro ${err.response.status}`);
+                setShowAlert(true);
+            });
+        setLoading(false);
+    }
+
+    const CustomAlert = ({ message, onClose }) => (
+        <AlertContainer>
+            <AlertBox>
+                <span>{message}</span>
+                <button onClick={onClose}>OK</button>
+            </AlertBox>
+        </AlertContainer>
+    );
+
+    const closeAlert = () => {
+        setShowAlert(false);
+    };
+
     return (
-        <SignUpContainer>
-            Página em construção, volte mais tarde
-        </SignUpContainer>
+        <SignupContainer>
+            <SignupForm>
+                <input
+                    type="text"
+                    placeholder="Nome"
+                    value={signupName}
+                    onChange={(event) => setName(event.target.value)}
+                    disabled={loading}
+                />
+                <input
+                    type="text"
+                    placeholder="CPF"
+                    value={formatCpf(signupCpf)}
+                    onChange={(e) =>
+                        setCpf(
+                            e.target.value.length < 14
+                                ? e.target.value
+                                : e.target.value.substring(0, 14)
+                        )
+                    }
+                    disabled={loading}
+                />
+                <input
+                    type="text"
+                    placeholder="E-mail"
+                    autoComplete="new-email"
+                    value={signupEmail}
+                    onChange={(event) => setEmail(event.target.value)}
+                    disabled={loading}
+                />
+                <input
+                    type="password"
+                    placeholder="Senha"
+                    autoComplete="new-password"
+                    value={signupPassword}
+                    onChange={(event) => setPassword(event.target.value)}
+                    disabled={loading}
+                />
+                <button onClick={sendSignup} disabled={loading}>
+                    {loading ? `...` : `CADASTRAR`}
+                </button>
+                <Link to={`/`}>Já possui conta? Entre</Link>
+            </SignupForm>
+            {showAlert && (
+                <CustomAlert message={alertMsg} onClose={closeAlert} />
+            )}
+        </SignupContainer>
     );
 }
 
-const SignUpContainer = styled.div`
+const SignupContainer = styled.div`
+    img {
+        width: 300px;
+    }
+    background-color: #0e0e13;
     font-size: 30px;
-    padding: 100px 18px;
+    padding: 0 38px;
     display: flex;
     flex-direction: column;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    font-size: 24px;
+    text-align: center;
+    height: 100vh;
+`;
+
+const SignupForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    gap: 16px;
+`;
+
+const AlertContainer = styled.div`
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    z-index: 10;
+`;
+
+const AlertBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-family: "Roboto";
+    font-size: 20px;
+    background-color: white;
+    border-radius: 8px;
+    padding: 12px;
+    width: 300px;
+    button {
+        margin-top: 8px;
+        padding-top: 4px;
+        justify-content: center;
+        align-items: center;
+        width: 15%;
+        height: 28px;
+    }
 `;
--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -3,15 +3,34 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
     * {
         box-sizing: border-box;
+        font-family: 'Roboto';
     }
     a:visited { text-decoration: none; }
     a:hover { text-decoration: none; }
     a:focus { text-decoration: none; }
     a:active { text-decoration: none; }
-    a { 
-        color: inherit;
-        text-decoration: none;
-    }	
+    a {
+        text-decoration: underline;
+        color: white;
+        font-size: 14px;
+        font-weight: 400;
+        font-family: 'Roboto';
+    }
+    input {
+        height: 52px;
+        border-radius: 8px;
+        padding-left: 14px;
+        border: none;
+    }
+    button {
+        border-radius: 8px;
+        border: none;
+        height: 52px;
+        background-color: #ff4791;
+        color: white;
+        font-size: 14px;
+        font-weight: 700;
+    }
 `;
 
 export default GlobalStyle;
